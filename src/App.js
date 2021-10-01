261e55f1b0d298e00b98088a6f835bfe0c74ed8f
Aylin McGinnis <amcginnis@rsglab.com>
2021-10-01 19:47:54 -0400

    Try something new for cors error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <div className={stylesheet.app}>
       <main>
         <div>
-          <p className={stylesheet.intro}>Collection of minimal and timeless knit patterns for women. This was a personal project of mine using the <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelery API</a> to help group similar styled knits that are wearable and modern.</p>
+          <p className={stylesheet.intro}>Collection of minimal and timeless knit patterns for women. This was a personal project of mine using the <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelery API</a> to help group similar styled modern knits.</p>
         </div>
         <Gallery/>
       </main>
