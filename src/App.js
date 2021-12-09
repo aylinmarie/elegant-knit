699830534d7aa9dbfa99211293a66f009110066f
Aylin McGinnis <amcginnis@rsglab.com>
2021-12-09 16:03:35 -0500

    Add cors server side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, {useEffect} from 'react';
 import stylesheet from './App.module.css';
 import Gallery from './Gallery';
 
 const App = () => {
+
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/cors', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
+  
   return (
     <>
       <header>
@@ -10,7 +26,7 @@ const App = () => {
       <div className={stylesheet.app}>
       <main>
         <div>
-          <p className={stylesheet.intro}>Collection of minimal and timeless knit patterns for women. This was a personal project of mine using the <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelery API</a> to help group similar styled modern knits.</p>
+          <p className={stylesheet.intro}>Collection of minimal and timeless knit patterns for women. This was a personal project of mine using the <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelry API</a> to help group similar styled modern knits.</p>
         </div>
         <Gallery/>
       </main>
