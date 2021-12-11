e0b88d03a434fa7bfcd8f4022875fd9716bcd1ec
Aylin McGinnis <amcginnis@rsglab.com>
2021-12-10 22:16:59 -0500

    Remove backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import stylesheet from './App.module.css';
 import Gallery from './Gallery';
 
 const App = () => {
-
-  const makeAPICall = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/cors', {mode:'cors'});
-      const data = await response.json();
-      console.log({ data })
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-  useEffect(() => {
-    makeAPICall();
-  }, [])
-  
   return (
     <>
       <header>
