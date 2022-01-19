8d60d3b0023a492c0d8079f9b5de4198c90696f5
aylinmarie <hello@aylinmarie.co>
2022-01-18 20:12:24 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import stylesheet from './App.module.css';
 import Gallery from './Gallery';
 
 const App = () => {
+
+  const LINKS = [
+    {
+      name: "Aylin Marie",
+      url: "https://www.aylinmarie.co"
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/aylin_marie"
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/aylinmcg"
+    },
+  ]
+
   return (
     <>
       <header>
@@ -11,10 +27,20 @@ const App = () => {
       <div className={stylesheet.app}>
       <main>
         <div>
-          <p className={stylesheet.intro}>Collection of minimal and timeless knit patterns for women. This was a personal project of mine using the <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelry API</a> to help group similar styled modern knits.</p>
+          <p className={stylesheet.intro}>Collection of minimal and timeless knitting patterns.</p>
         </div>
         <Gallery/>
       </main>
+      <footer className={stylesheet.footer}>
+        <ul>
+          {LINKS.map(link => {
+            return (
+              <li key={link.name}><a href={link.url} target="_blank" rel="noreferrer">{link.name}</a></li>
+            );
+          })}
+        </ul>
+        <span>Built with <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a> and <a href="https://www.ravelry.com/groups/ravelry-apihttps://www.ravelry.com/groups/ravelry-api" target="_blank" rel="noreferrer">Ravelry API</a></span>
+      </footer>
       </div>
     </>
   );
